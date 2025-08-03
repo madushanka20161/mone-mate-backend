878ba2f9722de96832a69637f22fa5e039e7019c
madushanka2016 <madushankakahawa@gmail.com>
2025-08-03 23:27:33 +1000

    firebase imiplimentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import express from 'express';
+import * as functions from 'firebase-functions';
+
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export const api = functions.https.onRequest(server);
